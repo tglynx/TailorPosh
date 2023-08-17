2c6d6a8a64ff3103c3470ef06766614509ce0784
Michael Sommer <github@neuronetix.de>
2023-08-17 11:00:19 +0200

    wildcard expansion implemented colored prefixes implemented (docker-compose style) -recurse parameter added for wildcard handling

--- Get-Tailor.ps1
+++ Get-Tailor.ps1
@@ -1,47 +1,80 @@
 ﻿function Get-Tailor {
-    param
-    (
+    param (
         [Parameter(Mandatory=$true)]
         [string[]]$files,
+
+        [switch]$Recurse,
+
         [long]$tail = 1
     )
 
-    function highlight
-    {
-        Begin
-        {
-            $settings = Get-Content "Tailor.json" | ConvertFrom-Json
+    $colors = @("Yellow", "Cyan", "Magenta", "Green", "Blue")
+
+    function Get-Settings {
+        $localSettingsPath = "Tailor.json"
+        $templateSettingsPath = "$env:SystemXInstallRepository\templates\Tailor.json"
+
+        if (Test-Path $localSettingsPath) {
+            return Get-Content $localSettingsPath | ConvertFrom-Json
+        } elseif (Test-Path $templateSettingsPath) {
+            return Get-Content $templateSettingsPath | ConvertFrom-Json
+        } else {
+            Write-Host "No Tailor.json found in local directory or template path."
+            exit
         }
-        Process
-        {
+    }
+
+    function highlight {
+        Begin {
+            $settings = Get-Settings
+        }
+        Process {
+            $serviceName = (Split-Path $_.PSParentPath -Leaf)
+            $serviceColor = $colors[([math]::Abs($serviceName.GetHashCode()) % $colors.Length)]
+
             $matched = $false
+
+            Write-Host ("[$serviceName] " + $_.PSChildName ) -NoNewline -ForegroundColor $serviceColor
+
             foreach ($setting in $settings.rules) {
                 if ($_ -match $setting.match) {
                     $matched = $true
-                    Write-Host $_ -ForegroundColor $setting.color
+                    Write-Host (" $_") -ForegroundColor $($setting.color)
                     break
                 }
             }
-            if (-not $matched -and $settings.default.enabled)
-            {
-                Write-Host $_ -ForegroundColor $settings.default.color
+            if (-not $matched -and $settings.default.enabled) {                
+                    Write-Host (" $_") -ForegroundColor $($settings.default.color)
             }
         }
     }
 
-    workflow tailor
-    {
-        param
-        (
-            [string[]]$files,
+    workflow tailor {
+        param (
+            [string[]]$logFiles,
             [long]$tail
         )
-        foreach -parallel ($file in $files) 
-        {
-            Get-Content -Tail $tail $file -wait
+        foreach -parallel ($file in $logFiles) {
+            Get-Content -wait -tail $tail $file 
         }
     }
 
-    $ProgressPreference='SilentlyContinue'
-    tailor $files $tail | highlight
+    $ProgressPreference = 'SilentlyContinue'
+
+    $expandedLogFiles = @()
+
+    foreach ($file in $files) {
+        $fileItems = Get-ChildItem -Path $file -Recurse:$Recurse -File | Where-Object { Test-Path $_.FullName }
+        $expandedLogFiles += $fileItems | Select-Object -ExpandProperty FullName
+    }
+
+    if ($expandedLogFiles.Count -gt 0) {
+        tailor $expandedLogFiles $tail | highlight
+    } else {
+        Write-Host "No valid log files found."
+    }
+
 }
+
+# Usage example
+# Get-Tailor -files "C:\path\to\log1.log", "./log/*" -Recurse
